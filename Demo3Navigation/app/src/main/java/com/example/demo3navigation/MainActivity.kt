b83db6a317f37951ebe18feddcea6458a0aeaedb
bepmai <nguyenphgmaiii23@gmail.com>
2025-07-27 21:09:49 +0700

    navigation basic

--- Demo3Navigation/app/src/main/java/com/example/demo3navigation/MainActivity.kt
+++ Demo3Navigation/app/src/main/java/com/example/demo3navigation/MainActivity.kt
@@ -11,6 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.demo3navigation.ui.catalog.category.CategoryScreen
+import com.example.demo3navigation.ui.catalog.product.ProductDetailScreen
+import com.example.demo3navigation.ui.home.HomeScreen
 import com.example.demo3navigation.ui.theme.Demo3NavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,5 +34,45 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainApp() {
+    val navController = rememberNavController()
+    Demo3NavigationTheme {
+        NavHost(navController = navController, startDestination = "home"){
+            composable ("home"){
+                HomeScreen(
+                    openCategoryAction = {
+                        navController.navigate("category")
+                    },
+                    openMyAccountScreen = {}
+                ) { }
+            }
 
+            composable("category") {
+                CategoryScreen (openProductDetail = {
+                    produceId ->
+                    navController.navigate("productDetail/$produceId")
+                })
+            }
+//category: arguments setBudlle prouducId
+//productDetail: lay ra arguments.getBudlle.getStrin...
+            composable(route = "productDetail/{produceId}",
+                arguments = listOf(
+                    navArgument(name = "produceId"){
+                        type = NavType.StringType
+                    }
+                )
+            ) {
+                backStackEntry ->
+                val produceId = backStackEntry.arguments?.getString("produceId")
+                requireNotNull(produceId)
+                ProductDetailScreen(productId = produceId, checkout = {
+                    cartId, customerId ->
+                })
+            }
+
+        }
+    }
 }
+
+//home: Màn hình chính (HomeScreen)
+//category: Màn hình danh mục sản phẩm (CategoryScreen)
+//productDetail/{produceId}: Màn hình chi tiết sản phẩm, truyền theo produceId
--- Demo3Navigation/app/src/main/java/com/example/demo3navigation/base/Extensions.kt
+++ Demo3Navigation/app/src/main/java/com/example/demo3navigation/base/Extensions.kt
@@ -1,8 +1,8 @@
 package com.example.demo3navigation.base
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.navigation.NavController
 
 @Composable
 fun <T>NavController.GetResultOneTime(key:String,onResult:(T?)->Unit){
