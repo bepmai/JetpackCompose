faedd814704bc937fc56ebd832b892b854683344
bepmai <nguyenphgmaiii23@gmail.com>
2025-07-28 22:12:16 +0700

    navigation basic

--- Demo3Navigation/app/src/main/java/com/example/demo3navigation/MainActivity.kt
+++ Demo3Navigation/app/src/main/java/com/example/demo3navigation/MainActivity.kt
@@ -3,14 +3,7 @@ package com.example.demo3navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,6 +11,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.demo3navigation.ui.catalog.category.CategoryScreen
 import com.example.demo3navigation.ui.catalog.product.ProductDetailScreen
+import com.example.demo3navigation.ui.customer.MyAccountScreen
 import com.example.demo3navigation.ui.home.HomeScreen
 import com.example.demo3navigation.ui.theme.Demo3NavigationTheme
 
@@ -36,39 +30,40 @@ class MainActivity : ComponentActivity() {
 fun MainApp() {
     val navController = rememberNavController()
     Demo3NavigationTheme {
-        NavHost(navController = navController, startDestination = "home"){
-            composable ("home"){
+        NavHost(navController = navController, startDestination = "home") {
+            composable("home") {
                 HomeScreen(
                     openCategoryAction = {
                         navController.navigate("category")
                     },
-                    openMyAccountScreen = {}
+                    openMyAccountScreen = { navController.navigate("myAccount") }
                 ) { }
             }
 
             composable("category") {
-                CategoryScreen (openProductDetail = {
-                    produceId ->
+                CategoryScreen(openProductDetail = { produceId ->
                     navController.navigate("productDetail/$produceId")
                 })
             }
-//category: arguments setBudlle prouducId
-//productDetail: lay ra arguments.getBudlle.getStrin...
             composable(route = "productDetail/{produceId}",
                 arguments = listOf(
-                    navArgument(name = "produceId"){
+                    navArgument(name = "produceId") {
                         type = NavType.StringType
                     }
                 )
-            ) {
-                backStackEntry ->
+            ) { backStackEntry ->
                 val produceId = backStackEntry.arguments?.getString("produceId")
                 requireNotNull(produceId)
-                ProductDetailScreen(productId = produceId, checkout = {
-                    cartId, customerId ->
+                ProductDetailScreen(productId = produceId, checkout = { cartId, customerId ->
                 })
             }
 
+            composable("myAccount") {
+                MyAccountScreen(
+                    openAddressScreen = {},
+                    navController = TODO()
+                )
+            }
         }
     }
 }
